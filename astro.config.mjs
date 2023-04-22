4df934441159edd607d1081a4adaeb2c2d74a0dc
BekmurzinTimur <49587448+BekmurzinTimur@users.noreply.github.com>
2023-04-22 14:17:28 +0300

    Added tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
-
 import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [solidJs()]
+  integrations: [solidJs(), tailwind()]
 });
